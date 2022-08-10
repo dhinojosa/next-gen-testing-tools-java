8e13b28f5037c673d3e3b71b3bb47487381d9042
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-08-10 15:09:28 -0500

    Converted package to evolutionnext and added jqwik database to file system. Renamed project

--- src/main/java/com/evolutionnext/Wallet.java
+++ src/main/java/com/evolutionnext/Wallet.java
@@ -1,4 +1,4 @@
-package com.xyzcorp;
+package com.evolutionnext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/test/java/com/evolutionnext/WalletTest.java
+++ src/test/java/com/evolutionnext/WalletTest.java
@@ -1,4 +1,4 @@
-package com.xyzcorp;
+package com.evolutionnext;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
